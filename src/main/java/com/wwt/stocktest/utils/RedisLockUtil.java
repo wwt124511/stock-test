83cd3cb8f182c3b4f88f3a914cf9dd28d15225dc
wwt124511 <wwt124511@users.noreply.github.com>
2022-12-17 02:23:54 +0800

    Create RedisLockUtil.java
    上传redis分布式锁工具类

---
+++ src/main/java/com/wwt/stocktest/utils/RedisLockUtil.java
@@ -0,0 +1,10 @@
+package com.wwt.stocktest.utils;
+
+/**
+ * @Author wwt
+ * @ClassName RedisLockUtil.java
+ * @Description redis分布式锁工具类
+ * @CreateTime 2022-12-17 02:23
+ */
+public class RedisLockUtil {
+}
